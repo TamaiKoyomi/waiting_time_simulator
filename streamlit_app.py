f880c19ce6ac10a613f462a53a0b7f53f024ad92
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-09-11 09:05:36 +0000

    adsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 #　メニューごとに関数設定　初期画面と待ちボタン画面とアンケート画面と結果画面
 
-number = 0
+st.session_state.number = 0
 
 def menu():
     st.title('待ち時間シミュレーター')
@@ -14,13 +14,13 @@ def menu():
 def count():
     st.write('自分の後ろに1人(1グループ)並ぶ度に下のボタンを押してください。')
     if st.button('人が来た'):
-        number += 1
+        st.session_state.number += 1
 
-    st.write('現在ボタンを押した回数:' + str(number) + '回')
+    st.write('現在ボタンを押した回数:' + str(st.session_state.number) + '回')
 
     if st.button('1人分取り消す'):
-        if number > 0:
-            number -= 1
+        if st.session_state.number > 0:
+            st.session_state.number -= 1
 
 
-menu()
+count()
