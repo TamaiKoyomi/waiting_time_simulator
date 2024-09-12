48fd647d13cd2f1a442240736b145b0277d6b123
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-09-12 07:49:57 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,17 @@ def menu():
 
     col1,col2 = st.colmuns
 
-    st.session_state.number = 0
-    st.title('待ち時間シミュレーター')
-    st.write('スタートボタンを押して、待ち時間をシミュレーションしてみましょう!')
-    if st.button('スタート'):
-        count()
+    with col1:
+        st.session_state.number = 0
+        st.title('待ち時間シミュレーター')
+        st.write('スタートボタンを押して、待ち時間をシミュレーションしてみましょう!')
     
+    with col2:
+        st.write('自分の後ろに1人(1グループ)並ぶ度に下のボタンを押してください。')
+        if st.button('人が来た'):
+            st.session_state.number += 1
 
-def count():
-    st.write('自分の後ろに1人(1グループ)並ぶ度に下のボタンを押してください。')
-    if st.button('人が来た'):
-        st.session_state.number += 1
-
-    st.write('現在ボタンを押した回数:' + str(st.session_state.number) + '回')
+        st.write('現在ボタンを押した回数:' + str(st.session_state.number) + '回')
 
     if st.button('1人分取り消す'):
         if st.session_state.number > 0:
