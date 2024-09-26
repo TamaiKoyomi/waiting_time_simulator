065a5c08da5b85281185149b7e42b00bbd8d6be7
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-09-26 06:06:24 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import time
+from time import sleep
 
 #　メニューごとに関数設定　初期画面と待ちボタン画面とアンケート画面と結果画面
 
@@ -7,15 +9,28 @@ if 'number' not in st.session_state:
     st.session_state.number = number
 
 def menu():
+
     #col1,col2 = st.columns
 
     #with col1:
+
+    target_time = 60
+
+    def up_timer(secs):
+        for i in range(secs, -1, -1):
+            print(i)
+            sleep(1)
+    print("時間です！")
 
     st.title('待ち時間シミュレーター')
     st.write('スタートボタンを押して、待ち時間をシミュレーションしてみましょう!')
     
     #with col2:
     st.write('時間いっぱい、1グループ進むたびに以下のボタンを押してください。')
+    
+    if st.button('1分間タイマースタート'):
+        up_timer(target_time)
+        
     if st.button('進んだ'):
         st.session_state.number += 1
 
