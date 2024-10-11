3bae8129c8b3d3b81d5c6eb84150e9971b5cf792
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-10-11 08:57:12 +0000

    あｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,50 @@
 import streamlit as st
 import time
 
-# import asyncio
-# import threading
-
 st.title('待ち時間シミュレーター')
 
-# セッションステートの初期化
-# if 'number' not in st.session_state:
-#    st.session_state.number = 0
+def menu():
+    st.write('並ぶ前に何分くらい待つのか知りたいですか? それとも、今並んでいる行列があと何分で自分の番になるのか知りたいですか?')
+    col1,col2 = st.columns
 
-#def async_timer(seconds):
-#    """スレッド内で実行するための関数"""
-#    asyncio.run(async_timer_inner(seconds))
+    with col1:
+        if st.button('並ぶ前'):
+            st.session_state.screen = 1
 
-# async def async_timer_inner(seconds):
-#    print(f'非同期タイマー開始：{seconds}秒')
-#    await asyncio.sleep(seconds)
-#    print(f'非同期タイマー終了：{seconds}秒')
+    with col2:
+        if st.button('並んだ後'):
+            st.session_state.screen = 2
 
-if 'start_time' not in st.session_state:
-    st.session_state.start_time = None
-    st.session_state.elapsed_time = 0
-
-if st.button('スタート'):
-    st.session_state.start_time = time.time()
-    st.session_state.elapsed_time = 0
-
-st.write('デバッグ'+str(st.session_state.start_time))
-
-if st.button('ストップ'):
-    if st.session_state.start_time is not None:
-        st.session_state.elapsed_time += time.time() - st.session_state.start_time
+def front_bef():
+    st.write('スタートボタンを押してから、3グループ分進んだらストップボタンを押してください。')
+    if 'start_time' not in st.session_state:
         st.session_state.start_time = None
+        st.session_state.elapsed_time = 0
 
-if st.session_state.start_time is not None:
-    elapsed = st.session_state.elapsed_time + (time.time() - st.session_state.start_time)
-else:
-    elapsed = st.session_state.elapsed_time
+    if st.button('スタート'):
+        st.session_state.start_time = time.time()
+        st.session_state.elapsed_time = 0
 
-st.write(f"経過時間:{elapsed:.2f}秒")
+    if st.button('ストップ'):
+        if st.session_state.start_time is not None:
+            st.session_state.elapsed_time += time.time() - st.session_state.start_time
+            st.session_state.start_time = None
 
-# def menu():
-#    st.title('待ち時間シミュレーター')
-#    st.write('スタートボタンを押して、待ち時間をシミュレーションしてみましょう!')
+    if st.session_state.start_time is not None:
+        elapsed = st.session_state.elapsed_time + (time.time() - st.session_state.start_time)
+        st.session_state.inf = elapsed
+        screen = 2
+    else:
+        elapsed = st.session_state.elapsed_time
 
-#    st.write('時間いっぱい、1グループ進むたびに以下のボタンを押してください。')
+    st.write(f"経過時間:{elapsed:.2f}秒")
 
- #   if st.button('1分間タイマースタート'):
-#        t1 = threading.Thread(target=async_timer, args=(60,))
-#        t1.start()  # スレッドを開始
+if 'screen' not in st.session_state:
+    st.screen = 0
 
- #   if st.button('進んだ'):
-#        st.session_state.number += 1
-
-#    st.write('現在ボタンを押した回数: ' + str(st.session_state.number) + '回')
-
-#    if st.button('1グループ分取り消す'):
-#        if st.session_state.number > 0:
-#            st.session_state.number -= 1
-
-#menu()'''
+if st.screen == 0:
+    menu()
+elif st.screen == 1:
+    front_bef()
+elif st.screen == 2:
+    st.write('うわあああ')
