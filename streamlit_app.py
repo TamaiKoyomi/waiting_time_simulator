18018bf860f5e1682a378b86143f33306869fe67
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-10-11 09:00:35 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,8 @@ st.title('待ち時間シミュレーター')
 
 def menu():
     st.write('並ぶ前に何分くらい待つのか知りたいですか? それとも、今並んでいる行列があと何分で自分の番になるのか知りたいですか?')
-    col1,col2 = st.columns
-
-    with col1:
-        if st.button('並ぶ前'):
-            st.session_state.screen = 1
-
-    with col2:
-        if st.button('並んだ後'):
-            st.session_state.screen = 2
+    if st.button('前'):
+        st.session_state.screen = 1
 
 def front_bef():
     st.write('スタートボタンを押してから、3グループ分進んだらストップボタンを押してください。')
@@ -33,18 +26,18 @@ def front_bef():
     if st.session_state.start_time is not None:
         elapsed = st.session_state.elapsed_time + (time.time() - st.session_state.start_time)
         st.session_state.inf = elapsed
-        screen = 2
+        st.session_state.screen = 2
     else:
         elapsed = st.session_state.elapsed_time
 
     st.write(f"経過時間:{elapsed:.2f}秒")
 
 if 'screen' not in st.session_state:
-    st.screen = 0
+    st.session_state.screen = 0
 
-if st.screen == 0:
+if st.session_state.screen == 0:
     menu()
-elif st.screen == 1:
+elif st.session_state.screen == 1:
     front_bef()
-elif st.screen == 2:
+elif st.session_state.screen == 2:
     st.write('うわあああ')
