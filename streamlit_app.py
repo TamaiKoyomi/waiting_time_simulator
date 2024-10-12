5018021cd541301d652d46c3aed898546852e72a
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-10-12 03:55:40 +0000

    あｓｆｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,30 @@ def front_bef():
             st.session_state.inf = elapsed
             st.session_state.screen = 2
 
+def back_bef():
+    st.write('スタートボタンを押してから、3グループ並んだらストップボタンを押してください。')
+
+    if 'start_time' not in st.session_state:
+        st.session_state.start_time = None
+        st.session_state.elapsed_time = 0
+
+    if st.button('スタート'):
+        st.session_state.start_time = time.time()
+        st.session_state.elapsed_time = 0
+    
+    if st.session_state.start_time is not None:
+        elapsed = st.session_state.elapsed_time + (time.time() - st.session_state.start_time)
+        st.write('3グループ目が並ぶまで、しばしお待ちください……')
+    else:
+        elapsed = st.session_state.elapsed_time
+
+    if st.button('ストップ'):
+        if st.session_state.start_time is not None:
+            st.session_state.elapsed_time += time.time() - st.session_state.start_time
+            st.session_state.start_time = None
+            st.session_state.back = elapsed
+            st.session_state.screen = 3
+
 if 'screen' not in st.session_state:
     st.session_state.screen = 0
 
@@ -40,4 +64,6 @@ if st.session_state.screen == 0:
 elif st.session_state.screen == 1:
     front_bef()
 elif st.session_state.screen == 2:
-    st.write('ご回答ありがとうございます!')
+    back_bef()
+elif st.session_state.screen == 3:
+    st.write('計算結果の処理ページ')
