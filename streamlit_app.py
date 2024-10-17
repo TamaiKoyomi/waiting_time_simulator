6916e9d71c02abded9fcafde96ac095578b9ce7e
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-10-17 07:49:38 +0000

    adsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def cal():
     if r >= 1:
         st.session_state.screen = 4
     if result < 1:
-        st.write('この列の平均待ち時間 ' + result + '秒')
+        st.write('この列の平均待ち時間 ' + str(result) + '秒')
     else:
         st.write('この列の平均待ち時間 ' + str(result) + '秒')
 
